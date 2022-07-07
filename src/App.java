8e02a24bb915a34749e2c924de34c22b7e5ab246
Bmattholder <bmattholder@gmail.com>
2022-07-06 20:37:40 -0500

    scanners in java

--- src/App.java
+++ src/App.java
@@ -1,17 +1,20 @@
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         
-        String x = "water";
-        String y = "Kool-Aid";
-        String temp;
+        Scanner scanner = new Scanner(System.in);
 
-        temp=x;
-        x=y;
-        y=temp;
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
 
-        System.out.println("x: "+x);
-        System.out.println("y: "+y);
+        System.out.println("Hello " + name + "!");
+        System.out.println("You are " + age + " years old.");
+        System.out.println("Your favorite food is " + food + ".");
     }
 }
