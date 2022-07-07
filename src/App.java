5be384037e56dfee05af17fe6a402dd15268bc4e
Bmattholder <bmattholder@gmail.com>
2022-07-06 20:27:00 -0500

    swap values of variables

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        
+        String x = "water";
+        String y = "Kool-Aid";
+        String temp;
+
+        temp=x;
+        x=y;
+        y=temp;
+
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
     }
 }
